93ffccc611fe7c9dfc0f0ce839c4a63fe8dbfef6
JhonAlexanderRoperoMojica-ADSI <jropero111@gmail.com>
2022-11-03 12:16:54 -0500

    unto 4 correguido

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ addEventListener("DOMContentLoaded", (e) => {
         e.preventDefault();
         let meses = document.querySelector("#meses").value;
         let dato = new Date(meses);
-        let ano,mes,dia;
+        let year,mes,dia;
         var Pago = 10;
         if (!!dato.valueOf()) {
-            ano = dato.getFullYear();
+            year = dato.getFullYear();
             mes = dato.getMonth() + 1;
             dia = dato.getDate() + 1;
         }
@@ -34,13 +34,10 @@ addEventListener("DOMContentLoaded", (e) => {
         'Diciembre'];
 
         for(let i=0;i<20;i++){
-            var indice = new Date(ano, mes - 1, 16).getDay();
-            document.querySelector("#res").insertAdjacentHTML("beforeend",
-            (`${Semana[indice]} del ${dia} de ${Mes[new Date(ano, mes - 1, dia).getMonth()]} del ${(new Date(ano, mes - 1, dia).getMonth()==0) ? ++ano : ano} debe pagar ${pago}<br>`));
-            
-            Pago = Pago*2;
-            
-            mes ++;
+            var indice = new Date(year, mes - 1, 16).getDay();
+            document.querySelector("#res").insertAdjacentHTML("beforeend",(`${Semana[indice]} del ${dia} de ${Mes[new Date(year, mes - 1, dia).getMonth()]} del ${(new Date(year, mes - 1, dia).getMonth()==0) ?++year : year} debe pagar ${Pago}<br>`));
+            Pago = Pago * 2;
+            mes++;
         }
     })
 })
