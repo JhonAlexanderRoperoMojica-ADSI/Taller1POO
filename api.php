c2c555c967e22abf164513e1f0c6cc9dea3cbbad
JhonAlexanderRoperoMojica-ADSI <jropero111@gmail.com>
2022-11-03 12:15:06 -0500

    Punto 4

--- api.php
+++ api.php
@@ -3,59 +3,9 @@
     header('Content-Type: application/json; charset=utf-8');
     $_DATA = json_decode(file_get_contents("php://input"),true);
 
-    class promedio{
-        public $sumaA;
-        public $cantidadA;
-        public $sumaB;
-        public $cantidadB;
-        public $sumaC;
-        public $cantidadC;
-
-        public function __construct(int $sumaA,int $cantidadA,int $sumaB,int $cantidadB, int $sumaC,int $cantidadC){
-            $this->sumaA = $sumaA;
-            $this->cantidadA = $cantidadA;
-            $this->sumaB = $sumaB;
-            $this->cantidadB = $cantidadB;
-            $this->sumaC = $sumaC;
-            $this->cantidadC = $cantidadC;
-        }
-
-        public function promedioF():string{
-
-            $sumaA = $this->sumaA;
-            $cantidadA = $this->cantidadA;
-            $sumaB = $this->sumaB;
-            $cantidadB = $this->cantidadB;
-            $sumaC = $this->sumaC;
-            $cantidadC = $this->cantidadC;
-
-            $cantidadA<1?$proA=0:$proA = $sumaA/$cantidadA;
-            $cantidadB<1?$proB=0:$proB = $sumaB/$cantidadB;
-            $cantidadC<1?$proC=0:$proC = $sumaC/$cantidadC;
-
-            if($proA>$proB && $proA>$proC){
-                if($proB>$proC){
-                    return "C";
-                } else{
-                    return "B";
-                }
-            } else if($proB>$proA && $proB>$proC){
-                if($proA>$proC){
-                    return "C";
-                } else{
-                    return "A";
-                }
-            } else if($proC>$proA && $proC>$proB){
-                if($proA>$proB){
-                    return "B";
-                } else{
-                    return "A";
-                }
-            } else{
-                return "A, B, C";
-            }
+    class Pago{
+        public function __construct(){
         }
     }
-    $lista = new promedio(sumaA:$_DATA[0],cantidadA:$_DATA[1],sumaB:$_DATA[2],cantidadB:$_DATA[3],sumaC:$_DATA[4],cantidadC:$_DATA[5]);
-    echo($lista->promedioF());
+    $lista = new Pago();
 ?>
--- main.js
+++ main.js
@@ -1,80 +1,46 @@
-let sumaA=0,sumaB=0,sumaC=0,cantidadA=0,cantidadB=0,cantidadC=0;
-
-function sumaPro(genero, carrera, edad) {
-    if(genero=="masculino"){
-        if(carrera=="a"){
-            sumaA= sumaA+edad;
-            cantidadA +=1;
-        }else if (carrera=="b"){
-            sumaB= sumaB+edad;
-            cantidadB +=1;
-        }else{
-            sumaC= sumaC+edad;
-            cantidadC +=1;
-        }
-    } else{
-        null
-    }
-}
-
 addEventListener("DOMContentLoaded", (e) => {
-    document.querySelector("#cantidad").removeAttribute("disabled");
-    document.querySelector("#cantidad").addEventListener("keyup", (e) => {
-        let genero = document.querySelector("#genero");
-        let edad = document.querySelector("#edad");
-        let carrera = document.querySelector("#carrera");
-        if (e.target.value > 0) {
-            genero.removeAttribute("disabled");
-            edad.removeAttribute("disabled");
-            carrera.removeAttribute("disabled");
-        } else {
-            genero.disabled = true;
-            edad.disabled = true;
-            carrera.disabled = true;
-        }
-    })
-    let con = 1;
-    let lista = [];
-    let myform = document.querySelector("#form");
-    myform.addEventListener("submit", async (e) => {
+    let form = document.querySelector("#form");
+    form.addEventListener("submit", async (e) => {
         e.preventDefault();
-        let cantidad = document.querySelector("#cantidad").value;
-        document.querySelector("#cantidad").disabled = true;
-        if (con < cantidad) {
-            let data = Object.fromEntries(new FormData(form));
-            sumaPro(data["genero"], data["carrera"], parseInt(data["edad"]));
-            document.querySelector("#genero").selectedIndex = 0;
-            document.querySelector("#edad").value = "";
-            document.querySelector("#carrera").selectedIndex = 0;
-            con += 1;
-
-        } else {
-            let form = e.target;
-            let data = Object.fromEntries(new FormData(form));
-            sumaPro(data["genero"], data["carrera"], parseInt(data["edad"]));
-            document.querySelector("#res").innerHTML = "";
-            lista.push(sumaA);
-            lista.push(cantidadA);
-            lista.push(sumaB);
-            lista.push(cantidadB);
-            lista.push(sumaC);
-            lista.push(cantidadC);
-            
-            let config = {
-                method: form.method,
-                body: JSON.stringify(lista)
-            };
-
-            let peticion = await fetch(form.action, config);
-            let respuesta = await peticion.text();
-            document.querySelector("#res").insertAdjacentHTML("beforeend", respuesta);
-            document.querySelector("#genero").selectedIndex = 0;
-            document.querySelector("#edad").value = "";
-            document.querySelector("#carrera").selectedIndex = 0;
-            genero.disabled = true;
-            edad.disabled = true;
-            carrera.disabled = true;
+        let meses = document.querySelector("#meses").value;
+        let dato = new Date(meses);
+        let ano,mes,dia;
+        var Pago = 10;
+        if (!!dato.valueOf()) {
+            ano = dato.getFullYear();
+            mes = dato.getMonth() + 1;
+            dia = dato.getDate() + 1;
         }
 
+        let Semana = ['Domingo', 
+        'Lunes', 
+        'Martes',
+        'Miércoles', 
+        'Jueves', 
+        'Viernes', 
+        'Sábado'];
+        
+        let Mes = ['Enero', 
+        'Febrero', 
+        'Marzo', 
+        'Abril', 
+        'Mayo', 
+        'Junio', 
+        'Julio', 
+        'Agosto', 
+        'Septiembre', 
+        'Octubre', 
+        'Noviembre', 
+        'Diciembre'];
+
+        for(let i=0;i<20;i++){
+            var indice = new Date(ano, mes - 1, 16).getDay();
+            document.querySelector("#res").insertAdjacentHTML("beforeend",
+            (`${Semana[indice]} del ${dia} de ${Mes[new Date(ano, mes - 1, dia).getMonth()]} del ${(new Date(ano, mes - 1, dia).getMonth()==0) ? ++ano : ano} debe pagar ${pago}<br>`));
+            
+            Pago = Pago*2;
+            
+            mes ++;
+        }
     })
 })
